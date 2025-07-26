c0070f1cb7bc0c70f1ece222e1bece1dacd67303
Amanda Birmingham <AmandaBirmingham@users.noreply.github.com>
2025-07-25 17:19:16 -0700

    rename tests for clarity, move mp_string tests into appropriate test file

--- metapool/tests/test_plate.py
+++ metapool/tests/test_plate.py
@@ -4,9 +4,6 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 
 from unittest import TestCase, main
-from metapool.mp_strings import (get_short_name_and_id, parse_project_name,
-                                 QIITA_ID_KEY, PROJECT_SHORT_NAME_KEY,
-                                 PROJECT_FULL_NAME_KEY)
 from metapool.plate import (_well_to_row_and_col, _decompress_well,
                             _plate_position, validate_plate_metadata,
                             _validate_plate, Message, ErrorMessage,
@@ -633,7 +630,7 @@ class PlateReplicationTests(TestCase):
         input_plate_fp = os.path.join(data_dir, 'data/input_plate.tsv')
         self.input_df = pd.read_csv(input_plate_fp, sep='\t', dtype=str)
 
-    def test_overwrite_source_quad(self):
+    def test_make_replicates_overwrite_source_quad(self):
         # replicate a valid source to empty sources 2 and 4 plus overwriting
         # source 3.
 
@@ -656,7 +653,7 @@ class PlateReplicationTests(TestCase):
                                                 'quadrant'):
             pr.make_replicates(self.input_df, {1: [2, 3, 4]}, overwrite=False)
 
-    def test_another_source(self):
+    def test_make_replicates_source_in_quad_3(self):
         # confirm that a source other than quad 1 can replicate to empty
         # sources.
         pr = PlateReplication('Library Well')
@@ -671,7 +668,7 @@ class PlateReplicationTests(TestCase):
 
         assert_frame_equal(obs, exp, check_like=True)
 
-    def test_two_replications(self):
+    def test_make_replicates_two_source_quads(self):
         # confirm that two sources can be replicated successfully.
         pr = PlateReplication('Library Well')
 
@@ -686,7 +683,7 @@ class PlateReplicationTests(TestCase):
 
         assert_frame_equal(obs, exp, check_like=True)
 
-    def test_parameter_conversion(self):
+    def test_make_replicates_non_list_input_params(self):
         # confirm conversion to lists works as intended.
         pr = PlateReplication('Library Well')
 
@@ -729,55 +726,6 @@ class PlateReplicationTests(TestCase):
                                                 'quadrant'):
             pr.make_replicates(self.input_df, {1: [2], 2: [1]},
                                overwrite=False)
-
-    def test_get_short_name_and_id(self):
-        # normal, non-pathological cases
-        obs = get_short_name_and_id('project_1')
-        self.assertEqual(2, len(obs))
-        self.assertEqual(obs[0], 'project')
-        self.assertEqual(obs[1], '1')
-
-        obs = get_short_name_and_id('project_00333333')
-        self.assertEqual(2, len(obs))
-        self.assertEqual(obs[0], 'project')
-        self.assertEqual(obs[1], '00333333')
-
-        # pathological cases
-        obs = get_short_name_and_id('project')
-        self.assertEqual(2, len(obs))
-        self.assertEqual(obs[0], 'project')
-        self.assertIsNone(obs[1])
-
-        obs = get_short_name_and_id('project_')
-        self.assertEqual(2, len(obs))
-        self.assertEqual(obs[0], 'project_')
-        self.assertIsNone(obs[1])
-
-    def test_parse_project_name_err_none(self):
-        expected_err = "project_name cannot be None or empty string"
-        with self.assertRaisesRegex(ValueError, expected_err):
-            parse_project_name(None)
-
-        with self.assertRaisesRegex(ValueError, expected_err):
-            parse_project_name("")
-
-    def test_parse_project_name_err_qiita_id(self):
-        with self.assertRaisesRegex(
-                ValueError, "'project' does not contain a Qiita-ID."):
-            parse_project_name("project")
-
-        with self.assertRaisesRegex(
-                ValueError, "'project_blue' does not contain a Qiita-ID."):
-            parse_project_name("project_blue")
-
-    def test_parse_project_name(self):
-        exp = {
-            QIITA_ID_KEY: '1',
-            PROJECT_SHORT_NAME_KEY: "project_green",
-            PROJECT_FULL_NAME_KEY: "project_green_1"
-        }
-        obs = parse_project_name("project_green_1")
-        self.assertDictEqual(exp, obs)
 
 
 class PlateRemapperTests(TestCase):
