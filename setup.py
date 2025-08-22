ef0084616278a954d21f09b0dd89df4db0584cc7
Amanda Birmingham <10677935+AmandaBirmingham@users.noreply.github.com>
2025-08-22 14:27:34 -0700

    remove dependencies info from setup.py in favor of environment.yml

--- setup.py
+++ setup.py
@@ -36,21 +36,6 @@ with open('README.md') as f:
 
 keywords = 'microbiome wetlab bioinformatics'
 
-base = ['qiita_client @ https://github.com/qiita-spots/qiita_client/archive/'
-        'master.zip', 'biom-format >= 2.1.16', 'click >= 8.1.7',
-        'matplotlib >= 3.9.2', 'numpy >= 2.0.2', 'openpyxl >= 3.1.5',
-        'pandas >= 2.2.3', 'sample-sheet >= 0.13.0', 'scikit-learn >= 1.5.2',
-        'seaborn >= 0.13.2']
-
-test = ['nose >= 1.3.7', 'pep8 >= 1.7.1', 'flake8 >= 7.1.1',
-        'papermill >= 2.6.0']
-
-coverage = ['coverage >= 7.6.8']
-
-notebook = ['jupyter >= 1.1.1', 'notebook >= 6.5.7', 'watermark >= 2.5.0']
-
-all_deps = base + test + coverage + notebook
-
 notebooks_fp = []
 for fp in glob('notebooks/*.ipynb'):
     notebooks_fp.append((dirname(fp), [fp]))
@@ -73,10 +58,6 @@ setup(name='metapool',
       include_package_data=True,
       # adding all the notebooks fps
       data_files=notebooks_fp,
-      install_requires=base,
-      extras_require={'test': test,
-                      'coverage': coverage,
-                      'all': all_deps},
       entry_points={
           'console_scripts': [
               'seqpro=metapool.scripts.seqpro:format_preparation_files',
