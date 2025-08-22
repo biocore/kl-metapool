089b99454451909b95483aed175af2e7dd37e0b3
Amanda Birmingham <10677935+AmandaBirmingham@users.noreply.github.com>
2025-08-22 09:33:14 -0700

    Remove unused imports

--- metapool/metapool.py
+++ metapool/metapool.py
@@ -6,17 +6,13 @@ import string
 import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
-from random import choices
-from configparser import ConfigParser
-from qiita_client import QiitaClient
 from .mp_strings import SAMPLE_NAME_KEY, PM_PROJECT_NAME_KEY, \
     PM_PROJECT_PLATE_KEY, PM_COMPRESSED_PLATE_NAME_KEY, PM_BLANK_KEY, \
     PLATE_NAME_DELIMITER, SAMPLE_DNA_CONC_KEY, NORMALIZED_DNA_VOL_KEY, \
     SYNDNA_POOL_MASS_NG_KEY, SYNDNA_POOL_NUM_KEY, TUBECODE_KEY, \
     EXTRACTED_GDNA_CONC_KEY, PM_WELL_KEY, PM_DILUTED_KEY, PM_WELL_ID_96_KEY, \
     NORMALIZED_WATER_VOL_KEY, CONTROLS_DESCRIPTION_KEY, \
-    get_qiita_id_from_project_name, get_plate_num_from_plate_name, \
-    get_main_project_from_plate_name
+    get_plate_num_from_plate_name, get_main_project_from_plate_name
 from .plate import _validate_well_id_96, PlateReplication, PlateRemapper, \
     merge_plate_dfs
 from .sequencers import is_i5_revcomp_sequencer
