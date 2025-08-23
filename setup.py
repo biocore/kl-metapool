fbc33e581ab1ea7bb41e5c714700688d5fbe6ec7
Amanda Birmingham <10677935+AmandaBirmingham@users.noreply.github.com>
2025-08-22 17:31:56 -0700

    add console scripts back to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
+from glob import glob
+from os.path import dirname
 import versioneer
+
+# collect notebook data files
+notebooks_fp = [(dirname(fp), [fp]) for fp in glob('notebooks/*.ipynb')]
 
 setup(
     name="metapool",
@@ -9,7 +14,22 @@ setup(
         exclude=("notebooks", "notebooks.*", "tests", "tests.*", "docs", "docs.*")
     ),
     include_package_data=True,
+    package_data={
+        # keep your data files
+        "metapool": ["data/*.tsv", "data/*.xlsx", "tests/data/*.csv"]
+    },
+    # include notebooks (if you still want them installed)
+    data_files=notebooks_fp,
+
+    # Conda-canonical: leave runtime deps to your conda recipe
+    # install_requires=[],
+
+    entry_points={
+        "console_scripts": [
+            "seqpro=metapool.scripts.seqpro:format_preparation_files",
+            "seqpro_mf=metapool.scripts.seqpro_mf:format_preparation_files_mf",
+        ],
+    },
+
     python_requires=">=3.9",
-    entry_points={"console_scripts": ["metapool=metapool.cli:main"]},
-    # install_requires=[],  # leave empty for conda-canonical
 )
