a2fe5175fecad8eb4c631429ac2f4397a8906172
Amanda Birmingham <10677935+AmandaBirmingham@users.noreply.github.com>
2025-08-29 11:33:56 -0700

    Add back setup and sync with environment

--- setup.py
+++ setup.py
@@ -1,36 +1,72 @@
+#!/usr/bin/env python
+
+# ----------------------------------------------------------------------------
+# Copyright (c) 2015--, metapool development team.
+#
+# Distributed under the terms of the Modified BSD License.
+# ----------------------------------------------------------------------------
+
 from setuptools import setup, find_packages
 from glob import glob
 from os.path import dirname
 import versioneer
 
+# NOTE: if you change this section,
+# ALSO change the environment.yml file
+base = [
+    'biom-format >= 2.1.16',
+    'matplotlib >= 3.9.2',
+    'numpy >= 2.0.2',
+    'openpyxl >= 3.1.5',
+    'pandas >= 2.2.3',
+    'seqtk >= 1.4',
+    'seaborn >= 0.13.2',
+    'scikit-learn >= 1.5.2',
+    # below can't be installed by conda
+    'sample-sheet >= 0.13.0']
+
+test = [
+    'flake8 >= 7.1.1',
+    'nose >= 1.3.7',
+    'papermill >= 2.6.0',
+    'pep8 >= 1.7.1']
+
+coverage = [
+    'coverage >= 7.6.8',
+    'coveralls']
+
+notebook = [
+    'jupyter >= 1.1.1',
+    'notebook >= 6.5.7',
+    'watermark >= 2.5.0']
+
+all_deps = base + test + coverage + notebook
+
 # collect notebook data files
-notebooks_fp = [(dirname(fp), [fp]) for fp in glob('notebooks/*.ipynb')]
+notebooks_fp = []
+for fp in glob('notebooks/*.ipynb'):
+    notebooks_fp.append((dirname(fp), [fp]))
 
 setup(
-    name="metapool",
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    packages=find_packages(
-        exclude=(
-            "notebooks", "notebooks.*", "tests", "tests.*", "docs", "docs.*")
-    ),
-    include_package_data=True,
-    package_data={
-        # keep your data files
-        "metapool": ["data/*.tsv", "data/*.xlsx", "tests/data/*.csv"]
-    },
-    # include notebooks (if you still want them installed)
-    data_files=notebooks_fp,
+    name='metapool',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      test_suite='nose.collector',
+      packages=find_packages(),
+      package_data={
+          'metapool': ['data/*.tsv', 'data/*.xlsx', 'tests/data/*.csv']},
+      include_package_data=True,
+      # adding all the notebooks fps
+      data_files=notebooks_fp,
+      install_requires=base,
+      extras_require={'test': test,
+                      'coverage': coverage,
+                      'all': all_deps},
+      entry_points={
+          'console_scripts': [
+              'seqpro=metapool.scripts.seqpro:format_preparation_files',
+              ('seqpro_mf=metapool.scripts.seqpro_mf:format_preparation_'
+               'files_mf'),
+          ],
 
-    # Conda-canonical: leave runtime deps to your conda recipe
-    # install_requires=[],
-
-    entry_points={
-        "console_scripts": [
-            "seqpro=metapool.scripts.seqpro:format_preparation_files",
-            "seqpro_mf=metapool.scripts.seqpro_mf:format_preparation_files_mf",
-        ],
-    },
-
-    python_requires=">=3.9",
-)
+      })
