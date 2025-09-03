4a83821b66f1372c311676c090d58135b58cf6c4
Amanda Birmingham <AmandaBirmingham@users.noreply.github.com>
2025-09-03 13:57:20 -0700

    Change well column name in PacBio sheets from 'well_id_384' to 'Sample_Well' (#330)
    * Change well column name in PacBio sheets from 'well_id_384' to 'Sample_Well'
    
    * fix lint issues
    
    ---------
    
    Co-authored-by: Amanda Birmingham <10677935+AmandaBirmingham@users.noreply.github.com>

--- metapool/sample_sheet.py
+++ metapool/sample_sheet.py
@@ -113,20 +113,26 @@ _CONTACT_COLS = MappingProxyType({
 _PREFIX_PLATE_COLUMNS = (SS_SAMPLE_ID_KEY, _SS_SAMPLE_NAME_KEY, 'Sample_Plate',
                          PM_WELL_ID_384_KEY)
 _SUFFIX_PLATE_COLUMNS = (_SS_SAMPLE_PROJECT_KEY, 'Well_description')
+_ILLUMINA_INDEX_COLUMNS = ('I7_Index_ID', 'index', 'I5_Index_ID', 'index2')
+_LC_ILLUMINA_INDEX_COLUMNS = tuple(
+    [x.lower() for x in _ILLUMINA_INDEX_COLUMNS])
 
 # Note that there doesn't appear to be a difference between 95, 99, and 100
 # beyond the value observed in 'Well_description' column. The real
 # difference is between standard_metag and abs_quant_metag.
 _BASE_DATA_COLUMNS = _PREFIX_PLATE_COLUMNS + \
-                     ('I7_Index_ID', 'index', 'I5_Index_ID', 'index2') + \
+                     _ILLUMINA_INDEX_COLUMNS + \
                      _SUFFIX_PLATE_COLUMNS
 
-_BASE_CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY, 'i5_index_id',
-                              'i7_index_id', 'index', 'index2',
-                              'library_construction_protocol',
-                              SAMPLE_NAME_KEY, 'sample_plate',
-                              'sample_project', 'well_description',
-                              PM_WELL_ID_384_KEY)
+_BASE_CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY,) + \
+    _LC_ILLUMINA_INDEX_COLUMNS + (
+        LIB_CONSTRUCT_PROTOCOL_KEY,
+        SAMPLE_NAME_KEY,
+        "sample_plate",
+        "sample_project",
+        "well_description",
+        PM_WELL_ID_384_KEY
+    )
 
 _BASE_GENERATED_PREP_COLUMNS = ('center_name', 'center_project_name',
                                 'instrument_model', 'lane', 'platform',
@@ -209,12 +215,16 @@ class KLSampleSheet(sample_sheet.SampleSheet):
                                      'Description': 'prep_description',
                                      'sample_plate': 'Sample_Plate'})
 
-    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY, 'i5_index_id',
-                             'i7_index_id', 'index', 'index2',
-                             'library_construction_protocol',
-                             SAMPLE_NAME_KEY, 'sample_plate',
-                             'sample_project', 'well_description',
-                             _SS_SAMPLE_WELL_KEY, _LANE_KEY)
+    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY,) + \
+        _LC_ILLUMINA_INDEX_COLUMNS + (
+            LIB_CONSTRUCT_PROTOCOL_KEY,
+            SAMPLE_NAME_KEY,
+            "sample_plate",
+            "sample_project",
+            "well_description",
+            _SS_SAMPLE_WELL_KEY,
+            _LANE_KEY,
+        )
 
     _GENERATED_PREP_COLUMNS = _BASE_GENERATED_PREP_COLUMNS
 
@@ -274,9 +284,8 @@ class KLSampleSheet(sample_sheet.SampleSheet):
         # _BASE_DATA_COLUMNS--the latter has well_id_384
         self._data_columns = (
             SS_SAMPLE_ID_KEY, _SS_SAMPLE_NAME_KEY, 'Sample_Plate',
-            _SS_SAMPLE_WELL_KEY,
-            'I7_Index_ID', 'index', 'I5_Index_ID', 'index2'
-        ) + _SUFFIX_PLATE_COLUMNS
+            _SS_SAMPLE_WELL_KEY
+            ) + _ILLUMINA_INDEX_COLUMNS + _SUFFIX_PLATE_COLUMNS
 
         self.Bioinformatics = None
         self.Contact = None
@@ -1422,13 +1431,6 @@ class KLSampleSheetWithReplicates(KLSampleSheet):
 
     _optional_replicate_columns = (ORIG_NAME_KEY, DESTINATION_WELL_384_KEY)
 
-    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY, 'i5_index_id',
-                             'i7_index_id', 'index', 'index2',
-                             LIB_CONSTRUCT_PROTOCOL_KEY,
-                             SAMPLE_NAME_KEY,
-                             'sample_plate', 'sample_project',
-                             'well_description', PM_WELL_ID_384_KEY)
-
     def __new__(cls, path=None, *args, **kwargs):
         """
             Override so that base class cannot be instantiated.
@@ -1456,6 +1458,7 @@ class KLSampleSheetWithReplicates(KLSampleSheet):
         super().__init__(path=path, defer_validate=True)
         self._remapper = _BASE_METAG_REMAPPER
         self._remapper = self._extend_mapping_type(self._REPLICATES_REMAPPER)
+        self._CARRIED_PREP_COLUMNS = _BASE_CARRIED_PREP_COLUMNS
         self._data_columns = _BASE_DATA_COLUMNS
         self._optional_col_sets = self._extend_mapping_type(
             {self._REPLICATES_SET_KEY: (
@@ -1516,7 +1519,7 @@ class KLSampleSheetWithSampleContext(KLSampleSheetWithReplicates):
         # it is defined here first.
         self.SampleContext = None
         super().__init__(path=path, defer_validate=True)
-        self._CARRIED_PREP_COLUMNS = _BASE_CARRIED_PREP_COLUMNS
+        # NB: inherits CARRIED_PREP_COLUMNS from KLSampleSheetWithReplicates
         self._validate_on_load(path, defer_validate)
 
 
@@ -1653,7 +1656,7 @@ class TellseqMetagSampleSheetv10(KLTellSeqSampleSheet):
     @property
     def CARRIED_PREP_COLUMNS(self):
         return [x for x in _BASE_CARRIED_PREP_COLUMNS if x not in
-                {'i7_index_id', 'index', 'index2', 'i5_index_id'}]
+                _LC_ILLUMINA_INDEX_COLUMNS]
 
 
 class TellseqAbsquantMetagSampleSheetv10(AbsQuantMixin, KLTellSeqSampleSheet):
@@ -1668,7 +1671,7 @@ class TellseqAbsquantMetagSampleSheetv10(AbsQuantMixin, KLTellSeqSampleSheet):
     @property
     def CARRIED_PREP_COLUMNS(self):
         return [x for x in _BASE_CARRIED_PREP_COLUMNS if x not in
-                {'i7_index_id', 'index', 'index2', 'i5_index_id'}] + \
+                _LC_ILLUMINA_INDEX_COLUMNS] + \
                 list(AbsQuantMixin._ABSQUANT_SPECIFIC_COLUMNS)
 
 
@@ -1688,12 +1691,15 @@ class AmpliconSampleSheet(KLSampleSheet):
         'Chemistry': 'Default',
     }
 
-    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY, 'i5_index_id',
-                             'i7_index_id', 'index', 'index2',
-                             LIB_CONSTRUCT_PROTOCOL_KEY,
-                             SAMPLE_NAME_KEY,
-                             'sample_plate', 'sample_project',
-                             'well_description', _SS_SAMPLE_WELL_KEY)
+    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY,) + \
+        _LC_ILLUMINA_INDEX_COLUMNS + (
+            LIB_CONSTRUCT_PROTOCOL_KEY,
+            SAMPLE_NAME_KEY,
+            'sample_plate',
+            'sample_project',
+            'well_description',
+            _SS_SAMPLE_WELL_KEY
+        )
 
     _ALLOW_MISSING_COLS = True
 
@@ -1729,7 +1735,7 @@ class PacBioSampleSheet(KLSampleSheetWithSampleContext):
             _SS_SAMPLE_PROJECT_KEY: str,
             _SS_QIITA_ID_KEY: str,
             _HUMAN_FILTERING_KEY: bool,
-            'library_construction_protocol': str,
+            LIB_CONSTRUCT_PROTOCOL_KEY: str,
             EXPT_DESIGN_DESC_KEY: str,
             CONTAINS_REPLICATES_KEY: bool
         }),
@@ -1738,10 +1744,13 @@ class PacBioSampleSheet(KLSampleSheetWithSampleContext):
     })
 
     _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY,
-                             'library_construction_protocol',
-                             SAMPLE_NAME_KEY, 'sample_plate',
-                             'sample_project', 'well_description',
-                             _SS_SAMPLE_WELL_KEY, _LANE_KEY)
+                             LIB_CONSTRUCT_PROTOCOL_KEY,
+                             SAMPLE_NAME_KEY,
+                             'sample_plate',
+                             'sample_project',
+                             'well_description',
+                             _SS_SAMPLE_WELL_KEY,
+                             _LANE_KEY)
 
     # No Settings or Reads sections
     _ordered_section_keys = (_HEADER_KEY, _DATA_KEY, _BIOINFORMATICS_KEY,
@@ -1780,10 +1789,16 @@ class PacBioSampleSheet(KLSampleSheetWithSampleContext):
         # it is defined here first.
         self.SampleContext = None
         super().__init__(path=path, defer_validate=True)
-        # No index columns in the remapper
+        # No index columns in the remapper, and well is "Sample_Well" not
+        # "well_id_384" because these are only done in 96-well plates
         self._remapper = _BASE_PLATE_REMAPPER
-        # No index or barcode columns in Data section
-        self._data_columns = _PREFIX_PLATE_COLUMNS + _SUFFIX_PLATE_COLUMNS
+        self._remapper = self._extend_mapping_type(
+            {"Well": _SS_SAMPLE_WELL_KEY})
+        # No index or barcode columns in Data section, and well column is
+        # "Sample_Well" instead of "well_id_384"
+        self._data_columns = (
+            SS_SAMPLE_ID_KEY, _SS_SAMPLE_NAME_KEY, 'Sample_Plate',
+            _SS_SAMPLE_WELL_KEY) + _SUFFIX_PLATE_COLUMNS
         self._validate_on_load(path, defer_validate)
 
 
@@ -1860,12 +1875,15 @@ class MetagenomicSampleSheetv90(KLSampleSheet):
 
     # data_columns are the same as base KLSampleSheet so they will not be
     # overridden here. _BIOINFORMATICS_COLUMNS as well.
-    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY, 'i5_index_id',
-                             'i7_index_id', 'index', 'index2',
-                             LIB_CONSTRUCT_PROTOCOL_KEY,
-                             SAMPLE_NAME_KEY,
-                             'sample_plate', 'sample_project',
-                             'well_description', 'Sample_Well')
+    _CARRIED_PREP_COLUMNS = (EXPT_DESIGN_DESC_KEY,) + \
+        _LC_ILLUMINA_INDEX_COLUMNS + (
+            LIB_CONSTRUCT_PROTOCOL_KEY,
+            SAMPLE_NAME_KEY,
+            "sample_plate",
+            "sample_project",
+            "well_description",
+            _SS_SAMPLE_WELL_KEY,
+        )
 
     def __init__(self, path=None, defer_validate=False):
         super().__init__(path=path, defer_validate=True)
@@ -1971,8 +1989,8 @@ class MetatranscriptomicSampleSheetv10(KLSampleSheet):
                 'Sample RNA Concentration': 'total_rna_concentration_ng_ul',
                 ELUTION_VOL_KEY: ELUTION_VOL_KEY
             }
-        self._data_columns = _PREFIX_PLATE_COLUMNS + \
-            ('I7_Index_ID', 'index', 'I5_Index_ID', 'index2',
+        self._data_columns = \
+            _PREFIX_PLATE_COLUMNS + _ILLUMINA_INDEX_COLUMNS + (
              _SS_SAMPLE_PROJECT_KEY, 'total_rna_concentration_ng_ul',
              ELUTION_VOL_KEY, 'Well_description')
         self._validate_on_load(path, defer_validate)
