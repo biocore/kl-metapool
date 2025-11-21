21b66b41c218de906a57c74f74da4d0a2e4d632c
Amanda Birmingham <AmandaBirmingham@users.noreply.github.com>
2025-11-21 10:01:01 -0800

    code review fix for exp_design)_desc, add date-zeroing to tests

--- notebooks/tests/test_pacbio_sample_sheet_builder.py
+++ notebooks/tests/test_pacbio_sample_sheet_builder.py
@@ -4,6 +4,7 @@ from notebooks.tests.notebook_test_helpers import TestNotebook
 
 class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
     NOTEBOOK = "pacbio_sample_sheet_builder.ipynb"
+    _ZERO_DATES_FUNC_KEY = "zero_dates_func"  # func to replace for dates
 
     def test_pacbio_v11_no_twist_metag(self):
         run_params = {
@@ -19,14 +20,17 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
             'twist_adaptor_id_col_key': None,
             'barcode_col_key': "Barcode",
             'run_id_key': "Sequencing Run",
-            'run_title_key': "Sequencing_Run_Name"
+            'run_title_key': "Sequencing_Run_Name",
+            'experimental_design_desc':
+                "fecal samples for metagenomic sequencing"
         }
 
         output_params = {
             'out_fp': {
                 self._OUT_PARAM_VARIABLE_KEY:
                     ('{path}/SampleSheets/pacbio_v11_metag_sample_sheet.csv'),
-                self._FILE_PATH_KEY: True
+                self._FILE_PATH_KEY: True,
+                self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
             }
         }
 
@@ -46,7 +50,9 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
             'twist_adaptor_id_col_key': "Amplification_Twist_UDI",
             'barcode_col_key': "Barcode",
             'run_id_key': "Sequencing Run",
-            'run_title_key': "Sequencing Run Name"
+            'run_title_key': "Sequencing Run Name",
+            'experimental_design_desc':
+                "fecal samples for metagenomic sequencing"
         }
 
         output_params = {
@@ -54,7 +60,8 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
                 self._OUT_PARAM_VARIABLE_KEY:
                     ('{path}/SampleSheets/'
                      'pacbio_v11_absquant_unpooled_sample_sheet.csv'),
-                self._FILE_PATH_KEY: True
+                self._FILE_PATH_KEY: True,
+                self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
             }
         }
 
@@ -74,7 +81,9 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
             'twist_adaptor_id_col_key': "Twist_UDI_Barcode",
             'barcode_col_key': "Barcode",
             'run_id_key': "Sequencing Run",
-            'run_title_key': "Sequencing Run Name"
+            'run_title_key': "Sequencing Run Name",
+            'experimental_design_desc':
+                "soil samples for metagenomic sequencing"
         }
 
         output_params = {
@@ -82,7 +91,8 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
                 self._OUT_PARAM_VARIABLE_KEY:
                     ('{path}/SampleSheets/'
                      'pacbio_v11_absquant_pooled_sample_sheet.csv'),
-                self._FILE_PATH_KEY: True
+                self._FILE_PATH_KEY: True,
+                self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
             }
         }
 
