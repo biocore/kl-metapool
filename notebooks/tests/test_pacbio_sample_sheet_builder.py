047c018a3c899be0100aee1690f84a1d7fc2e9fb
Amanda Birmingham <AmandaBirmingham@users.noreply.github.com>
2026-02-24 05:23:06 -0800

    update pacbio samplesheet notebook, tests, and test samplesheets (#365)
    * update pacbio samplesheet notebook, tests, and test samplesheets
    
    * make lint fixes

--- notebooks/tests/test_pacbio_sample_sheet_builder.py
+++ notebooks/tests/test_pacbio_sample_sheet_builder.py
@@ -6,11 +6,12 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
     NOTEBOOK = "pacbio_sample_sheet_builder.ipynb"
     _ZERO_DATES_FUNC_KEY = "zero_dates_func"  # func to replace for dates
 
-    def test_pacbio_v11_no_twist_metag(self):
+    def test_pacbio_v11_metag_case1_wo_amp_wo_absquant(self):
         run_params = {
             'processing_doc_fp': (
                 f'{self.test_data_dir}/processing_docs/'
-                f'pacbio_metag_processing_doc.csv'),
+                f'pacbio_v11_metag_case1_without_amp_without_'
+                f'absquant_processing_doc.csv'),
             'qiita_id_key': "Qiita_ID",
             'sample_name_col_key': "sample_name",
             'sample_type_key': "sample_type",
@@ -28,7 +29,8 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
         output_params = {
             'out_fp': {
                 self._OUT_PARAM_VARIABLE_KEY:
-                    ('{path}/SampleSheets/pacbio_v11_metag_sample_sheet.csv'),
+                    ('{path}/SampleSheets/pacbio_v11_metag_sample_sheet_'
+                     'case1_without_amp_without_absquant.csv'),
                 self._FILE_PATH_KEY: True,
                 self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
             }
@@ -36,18 +38,19 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
 
         self._run_notebook_test(run_params, output_params)
 
-    def test_pacbio_v11_unpooled_absquant(self):
+    def test_pacbio_v11_absquant_sample_sheet_case2_wo_amp_w_absquant(self):
         run_params = {
             'processing_doc_fp': (
                 f'{self.test_data_dir}/processing_docs/'
-                f'pacbio_twist_unpooled_absquant_processing_doc.csv'),
+                'pacbio_v11_absquant_case2_without_amp_'
+                'with_absquant_processing_doc.csv'),
             'qiita_id_key': "Qiita_ID",
             'sample_name_col_key': "sample_name",
             'sample_type_key': "sample_type",
             'plate_col_key': "Extraction_Plate",
             'project_name_col_key': "Project_Name",
             'well_col_key': "Library_Well_ID",
-            'twist_adaptor_id_col_key': "Amplification_Twist_UDI",
+            'twist_adaptor_id_col_key': None,
             'barcode_col_key': "Barcode",
             'run_id_key': "Sequencing Run",
             'run_title_key': "Sequencing Run Name",
@@ -59,7 +62,8 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
             'out_fp': {
                 self._OUT_PARAM_VARIABLE_KEY:
                     ('{path}/SampleSheets/'
-                     'pacbio_v11_absquant_unpooled_sample_sheet.csv'),
+                     'pacbio_v11_absquant_sample_sheet_case2_without_amp_'
+                     'with_absquant.csv'),
                 self._FILE_PATH_KEY: True,
                 self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
             }
@@ -67,11 +71,44 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
 
         self._run_notebook_test(run_params, output_params)
 
-    def test_pacbio_v11_pooled_absquant(self):
+    def test_pacbio_v11_metag_sample_sheet_case3_w_amp_wo_absquant(self):
         run_params = {
             'processing_doc_fp': (
                 f'{self.test_data_dir}/processing_docs/'
-                'pacbio_twist_pooled_absquant_processing_doc.csv'),
+                f'pacbio_v11_metag_case3_with_amp_without_'
+                f'absquant_processing_doc.csv'),
+            'qiita_id_key': "Qiita_ID",
+            'sample_name_col_key': "sample_name",
+            'sample_type_key': "sample_type",
+            'plate_col_key': "Extraction_Plate",
+            'project_name_col_key': "Project_Name",
+            'well_col_key': "Library_Well_ID",
+            'twist_adaptor_id_col_key': 'Twist_UDI_Barcode',
+            'barcode_col_key': "Barcode",
+            'run_id_key': "Sequencing Run",
+            'run_title_key': "Sequencing_Run_Name",
+            'experimental_design_desc':
+                "fecal samples for metagenomic sequencing"
+        }
+
+        output_params = {
+            'out_fp': {
+                self._OUT_PARAM_VARIABLE_KEY:
+                    ('{path}/SampleSheets/pacbio_v11_metag_sample_sheet_'
+                     'case3_with_amp_without_absquant.csv'),
+                self._FILE_PATH_KEY: True,
+                self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
+            }
+        }
+
+        self._run_notebook_test(run_params, output_params)
+
+    def test_pacbio_v11_absquant_case4_w_amp_w_absquant_syndna_before(self):
+        run_params = {
+            'processing_doc_fp': (
+                f'{self.test_data_dir}/processing_docs/'
+                f'pacbio_v11_absquant_case4_with_amp_with_absquant_syndna_'
+                f'before_processing_doc.csv'),
             'qiita_id_key': "Qiita_ID",
             'sample_name_col_key': "Sample_ID",
             'sample_type_key': "sample_type",
@@ -90,7 +127,41 @@ class TestPacbioSampleSheetBuilderNotebook(TestNotebook):
             'out_fp': {
                 self._OUT_PARAM_VARIABLE_KEY:
                     ('{path}/SampleSheets/'
-                     'pacbio_v11_absquant_pooled_sample_sheet.csv'),
+                     'pacbio_v11_absquant_sample_sheet_case4_with_amp_'
+                     'with_absquant_syndna_before.csv'),
+                self._FILE_PATH_KEY: True,
+                self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
+            }
+        }
+
+        self._run_notebook_test(run_params, output_params)
+
+    def test_pacbio_v11_absquant_case5_with_amp_with_absquant_syndna_after(self):  # noqa: E501
+        run_params = {
+            'processing_doc_fp': (
+                f'{self.test_data_dir}/processing_docs/'
+                f'pacbio_v11_absquant_case5_with_amp_with_absquant_syndna_'
+                f'after_processing_doc.csv'),
+            'qiita_id_key': "Qiita_ID",
+            'sample_name_col_key': "sample_name",
+            'sample_type_key': "sample_type",
+            'plate_col_key': "Extraction_Plate",
+            'project_name_col_key': "Project_Name",
+            'well_col_key': "Library_Well_ID",
+            'twist_adaptor_id_col_key': "Amplification_Twist_UDI",
+            'barcode_col_key': "Barcode",
+            'run_id_key': "Sequencing Run",
+            'run_title_key': "Sequencing Run Name",
+            'experimental_design_desc':
+                "fecal samples for metagenomic sequencing"
+        }
+
+        output_params = {
+            'out_fp': {
+                self._OUT_PARAM_VARIABLE_KEY:
+                    ('{path}/SampleSheets/'
+                     'pacbio_v11_absquant_sample_sheet_case5_with_amp_'
+                     'with_absquant_syndna_after.csv'),
                 self._FILE_PATH_KEY: True,
                 self._ZERO_DATES_FUNC_KEY: self._replace_illumina_date
             }
